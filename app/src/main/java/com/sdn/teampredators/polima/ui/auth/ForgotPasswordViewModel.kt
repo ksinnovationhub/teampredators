ef4be1f39123a71490d842b5bc12410f51286b5f
Segun Francis <francis4dblues@gmail.com>
2022-02-10 00:24:08 +0100

    * Add app icon * Edit onboarding button text * Implement forgot password logic [WIP]

---
+++ app/src/main/java/com/sdn/teampredators/polima/ui/auth/ForgotPasswordViewModel.kt
@@ -0,0 +1,31 @@
+package com.sdn.teampredators.polima.ui.auth
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.firebase.auth.FirebaseAuth
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import timber.log.Timber
+
+@HiltViewModel
+class ForgotPasswordViewModel @Inject constructor(private val auth: FirebaseAuth) : ViewModel() {
+
+    private val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
+        Timber.e(throwable)
+    }
+
+    fun recoverPassword(email: String) {
+        viewModelScope.launch(exceptionHandler) {
+            kotlin.runCatching {
+                auth.sendPasswordResetEmail(email).await()
+            }.onSuccess {
+
+            }.onFailure {
+
+            }
+        }
+    }
+}
